8602f6490569f03de5b36892f315254c769429d7
Adam Dicky <admdcky@gmail.com>
2025-09-14 19:05:41 +0800

    Added admin remark box at posts and edit post modal.

--- src/components/EditPostModal.tsx
+++ src/components/EditPostModal.tsx
@@ -6,44 +6,48 @@ import { Input } from "@/components/ui/input";
 import { Textarea } from "@/components/ui/textarea";
 import { Label } from "@/components/ui/label";
 import { Button } from "@/components/ui/button";
-import { UploadSimpleIcon, XCircleIcon } from "@phosphor-icons/react";
+import { UploadSimpleIcon, XCircleIcon, ChatIcon } from "@phosphor-icons/react";
+import RemarkBox from "./RemarkBox";
 
-interface CreatePostModalProps {
+interface EditPostModalProps {
     open: boolean;
     onOpenChange: (open: boolean) => void;
 }
 
-export default function CreatePostModal({ open, onOpenChange }: CreatePostModalProps) {
+export default function EditPostModal({ open, onOpenChange }: EditPostModalProps) {
     const [images, setImages] = useState<File[]>([]);
 
     return (
         <Dialog open={open} onOpenChange={onOpenChange}>
-            <DialogContent className="sm:max-w-[700px] overflow-y-auto max-h-[100vh] pr-2">
+            <DialogContent className="sm:max-w-[700px] overflow-y-auto max-h-[100vh]">
                 <DialogHeader>
-                    <DialogTitle className='text-2xl text-[#243056]'>Create a New Post</DialogTitle>
+                    <DialogTitle className='text-2xl text-[#243056]'>Edit Post</DialogTitle>
                     <DialogDescription className='leading-3'>
-                        Share your classroom's updates and achievements.
+                        Update your classroom's post details and images.
                     </DialogDescription>
                 </DialogHeader>
 
 
                 <div className="flex flex-col gap-5 py-2">
+                    {/* Remark, if available. */}
+                    <RemarkBox />
+
                     {/* Title */}
                     <div className="flex flex-col gap-1">
                         <Label htmlFor="title" className='text-[#243056] pb-1 font-semibold'>Title*</Label>
-                        <Input id="title" placeholder="Enter a descriptive title for your post.." />
+                        <Input id="title" placeholder="fetch title from db here.." />
                     </div>
 
                     {/* Content */}
                     <div className="flex flex-col gap-1">
                         <Label htmlFor="content" className='text-[#243056] pb-1 font-semibold'>Description*</Label>
-                        <Textarea id="content" placeholder="Share the detials of the post.." rows={5} />
+                        <Textarea id="content" placeholder="fetch content from db here.." rows={5} />
                     </div>
 
                     {/* Tags */}
                     <div className="flex flex-col gap-1">
                         <Label htmlFor="tags" className='text-[#243056] pb-1 font-semibold'>Tags*</Label>
-                        <Input id="tags" placeholder="Add tags (e.g. Examination, Activity).." />
+                        <Input id="tags" placeholder="fetch tags from db here.." />
                     </div>
 
                     {/* File Upload (images) */}
@@ -67,7 +71,7 @@ export default function CreatePostModal({ open, onOpenChange }: CreatePostModalP
                             onClick={() => document.getElementById('images')?.click()}
                         >
                             <UploadSimpleIcon size={40} className='text-[#6D76D5]' />
-                            <a className='font-semibold text-[#243056]'>Upload Images</a>
+                            <a className='font-semibold text-[#243056]'>Delete or Upload More Images</a>
                             <a className='font-small text-[#6E7793] italic text-center'>Click here or drag and drop (3 images maximum)</a>
                         </div>
 
@@ -79,7 +83,7 @@ export default function CreatePostModal({ open, onOpenChange }: CreatePostModalP
 
 
                         {images.length > 0 && (
-                            <div className="grid grid-cols-3 gap-2 mt-2 max-h-50 overflow-y-auto pr-2">
+                            <div className="grid grid-cols-3 gap-2 mt-2 max-h-30 overflow-y-auto pr-2">
                                 {images.map((file, index) => (
                                     <div key={index} className="relative group">
                                         <img
@@ -103,12 +107,20 @@ export default function CreatePostModal({ open, onOpenChange }: CreatePostModalP
                                 ))}
                             </div>
                         )}
+
+                        {images.length > 3 && (
+                            <div className="absolute bottom-6 left-0 w-full  text-center py-2">
+                                <span className="text-sm text-[#6E7793] italic">
+                                    Scroll down for more pictures ↓
+                                </span>
+                            </div>
+                        )}
                     </div>
                 </div>
 
                 <DialogFooter>
                     <Button variant="outline" className='font-semibold hover:cursor-pointer' onClick={() => onOpenChange(false)}>Cancel</Button>
-                    <Button variant='outline' type="submit" className='font-semibold bg-[#6D76D5] text-white hover:cursor-pointer hover:text-white hover:bg-[#6570ea]'>Create Post</Button>
+                    <Button variant='outline' type="submit" className='font-semibold bg-[#6D76D5] text-white hover:cursor-pointer hover:text-white hover:bg-[#6570ea]'>Confirm Edit</Button>
                 </DialogFooter>
             </DialogContent>
 
---
+++ src/components/RemarkBox.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import { ChatIcon } from "@phosphor-icons/react";
+
+const RemarkBox = () => {
+    return (
+        <div className="flex flex-col p-3 gap-1 bg-[#E0F2FE] border-1 border-[#0095FF] rounded-md">
+            <div className="flex flex-row items-center gap-2">
+                <ChatIcon size={20} weight="bold" className='text-[#007BD3]' />
+                <a className="text-[#007BD3] font-semibold">Admin Remarks</a>
+            </div>
+            <a className="text-[#007BD3] font-medium  max-h-15 overflow-y-auto pr-2 leading-5">Please add specific time slots and room numbers for better clarity. Please add specific time slots and room numbers for better clarity. Please add specific time slots and room numbers for better clarity. Please add specific time slots and room numbers for better clarity.</a>
+        </div>
+    );
+};
+
+export default RemarkBox;
